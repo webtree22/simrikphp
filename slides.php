1957bc0b9a508889d2521b60167e13664d7a31b0
Sudeep Gurung <sudeepgurung.sg@gmail.com>
2021-08-05 16:53:29 +0545

    wip

--- slides.php
+++ slides.php
@@ -22,10 +22,12 @@
                 <div class="container position-relative z-index-3 h-100">
                     <div class="row align-items-center h-100">
                         <div class="col pb-4">
-                            <h1 class="text-color-light font-weight-extra-bold text-13 line-height-2 mb-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">MEDICAL APPOINTMENT</h1>
-                            <h2 class="text-color-light font-weight-extra-bold text-4-5 line-height-2 mb-3 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="750" data-plugin-options="{'minWindowWidth': 0}">SCHEDULE A MEDICAL APPOINTMENT NOW</h2>
-                            <div class="d-inline-block">
-                                <p class="text-color-light custom-border-bottom-1 font-weight-light text-4-5 mb-0 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}">Online or Over the Phone</p>
+                            <div style="background-color: black; padding: 30px; opacity: 0.4; display: inline-block">
+                                <h1 class="text-color-light font-weight-extra-bold text-13 line-height-2 mb-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">MEDICAL APPOINTMENT</h1>
+                                <h2 class="text-color-light font-weight-extra-bold text-4-5 line-height-2 mb-3 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="750" data-plugin-options="{'minWindowWidth': 0}">SCHEDULE A MEDICAL APPOINTMENT NOW</h2>
+                                <div class="d-inline-block">
+                                    <p class="text-color-light custom-border-bottom-1 font-weight-light text-4-5 mb-0 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}">Online or Over the Phone</p>
+                                </div>
                             </div>
                         </div>
                     </div>
